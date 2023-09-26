f6c5a1c49a6135d2f1529b153bd97adeba987351
Angel Puruncajas Guevara <angelefrain23@gmail.com>
2023-09-26 18:50:40 +0100

    Add initial code to dice game.

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var randomNumber1 = Math.floor((Math.random()) * 6) + 1;
+
+document.querySelector(".dice .img1").setAttribute("src", "/Dice Roll/images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor((Math.random()) * 6) + 1;
+
+document.querySelector(".dice .img2").setAttribute("src", "/Dice Roll/images/dice" + randomNumber2 + ".png");
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "🏆 Player 1 Wins!"
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").textContent = "Player 2 Wins 🏆!"
+} else {
+    document.querySelector("h1").textContent = "Draw!"
+}
+
+
+// Button refreshing JS
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    location.reload();
+}
+)
