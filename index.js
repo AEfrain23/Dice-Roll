d3db23b7fb94ef44a771ad578fef56a7b7141db1
AEfrain23 <108274105+AEfrain23@users.noreply.github.com>
2023-09-26 20:58:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 var randomNumber1 = Math.floor((Math.random()) * 6) + 1;
 
-document.querySelector(".dice .img1").setAttribute("src", "Dice-Roll/images/dice" + randomNumber1 + ".png");
+document.querySelector(".dice .img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
 var randomNumber2 = Math.floor((Math.random()) * 6) + 1;
 
-document.querySelector(".dice .img2").setAttribute("src", "/Dice-Roll/images/dice" + randomNumber2 + ".png");
+document.querySelector(".dice .img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 
 if (randomNumber1 > randomNumber2) {
