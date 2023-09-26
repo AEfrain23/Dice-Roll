09500723c9ccfc7c63a88a0a19fd34e1631809c7
AEfrain23 <108274105+AEfrain23@users.noreply.github.com>
2023-09-26 20:31:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var randomNumber1 = Math.floor((Math.random()) * 6) + 1;
 
-document.querySelector(".dice .img1").setAttribute("src", "/Dice-Roll/images/dice" + randomNumber1 + ".png");
+document.querySelector(".dice .img1").setAttribute("src", "Dice-Roll/images/dice" + randomNumber1 + ".png");
 
 var randomNumber2 = Math.floor((Math.random()) * 6) + 1;
 
